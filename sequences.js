81847a8801230b189f89610bd0a2425c3ee91a56
mwguthrie <stampeder@gmail.com>
2018-07-20 17:22:33 -0400

    Update sequences.js

--- sequences.js
+++ sequences.js
@@ -13,9 +13,7 @@ var colors = {
   "home": "#5687d1",
   "product": "#7b615c",
   "search": "#de783b",
-  "account": "#6ab975",
-  "other": "#a173d1",
-  "end": "#bbbbbb"
+  "account": "#6ab975"
 };
 
 // Total size of all segments; we set this later, after loading the data.
