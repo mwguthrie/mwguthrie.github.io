292ed983a6e9b5d93faba4466566ae6c3c83c616
mwguthrie <stampeder@gmail.com>
2018-07-22 20:45:49 -0400

    Update sequences.js

--- sequences.js
+++ sequences.js
@@ -55,7 +55,7 @@ var arc = d3.arc()
 
 // Use d3.text and d3.csvParseRows so that we do not need to have a header
 // row, and can receive the csv as an array of arrays.
-d3.text("sequences_WAKE.csv", function(text) {
+d3.text("sequences_PE.csv", function(text) {
   var csv = d3.csvParseRows(text);
   var json = buildHierarchy(csv);
   createVisualization(json);
