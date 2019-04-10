89ccd006ba670bd8248204f21c7eeb9a6b3560d1
mwguthrie <stampeder@gmail.com>
2019-04-10 12:37:46 -0400

    Update sequences.js

--- sequences.js
+++ sequences.js
@@ -21,8 +21,8 @@ var colors = {
   "Other": "#403f4c",
   "": "#ffffff",
   //"Kinetic Energy": "#ffffff"
-  "Work by a constant force": "#ffffff",
-  //"Work and Kinetic Energy": "#ffffff"
+  //"Work by a constant force": "#ffffff",
+  "Work and Kinetic Energy": "#ffffff"
   //"Potential Energy": "#ffffff"
   //"When is Mechanical Energy Conserved": "#ffffff",
   //"Simple Application of Conservation of Mechanical Energy": "#ffffff",
@@ -57,7 +57,7 @@ var arc = d3.arc()
 //////////////////////////////////////////////////////////////////////////////
 // Use d3.text and d3.csvParseRows so that we do not need to have a header
 // row, and can receive the csv as an array of arrays.
-d3.text("sunburstData_New_WBACF.csv", function(text) {
+d3.text("sunburstData_New_WAKE.csv", function(text) {
   var csv = d3.csvParseRows(text);
   var json = buildHierarchy(csv);
   createVisualization(json);
