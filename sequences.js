8591304094195fdbd18ff8d54c563d275e40ab57
mwguthrie <stampeder@gmail.com>
2019-04-10 13:27:37 -0400

    Update sequences.js

--- sequences.js
+++ sequences.js
@@ -28,8 +28,8 @@ var colors = {
   //"Simple Application of Conservation of Mechanical Energy": "#ffffff",
   //"Problems Using Conservation of Mechanical Energy": "#ffffff"
   //"Problems with Two Types of Potential Energy": "#ffffff",
-  "Mechanical Energy and Nonconservative Work": "#ffffff"
-  //"More mechanical energy problems": "#ffffff"
+  //"Mechanical Energy and Nonconservative Work": "#ffffff"
+  "More mechanical energy problems": "#ffffff"
 };
 
 // Total size of all segments; we set this later, after loading the data.
@@ -57,7 +57,7 @@ var arc = d3.arc()
 //////////////////////////////////////////////////////////////////////////////
 // Use d3.text and d3.csvParseRows so that we do not need to have a header
 // row, and can receive the csv as an array of arrays.
-d3.text("sunburstData_New_MEANW.csv", function(text) {
+d3.text("sunburstData_New_MMEP.csv", function(text) {
   var csv = d3.csvParseRows(text);
   var json = buildHierarchy(csv);
   createVisualization(json);
